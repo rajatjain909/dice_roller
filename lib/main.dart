4b12d1c6eac534b51990ad13141daec8b5364ecc
rajatsparktg <rajat@sparktg.com>
2024-04-07 03:53:37 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:learning_one/gradient_container.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  const MaterialApp(
+      // title: 'Flutter Demo',
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //   useMaterial3: true,
+      // ),
+      home: Scaffold(
+        body: 
+        // GradientContainer(
+        //   Color.fromARGB(255, 26, 2, 80),
+        //   Color.fromARGB(255, 45, 7, 98),
+        // ),
+        GradientContainer.purple(),
+      ),
+    );
+  }
+}
